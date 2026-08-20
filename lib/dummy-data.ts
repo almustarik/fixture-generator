5b5b18e8c3ebd9018f6e46fc40963b397c5d6506
Al Mustarik <almustarik@gmail.com>
2026-08-20 12:00:13 +0600

    feat: add dummy data for players and round title

---
+++ lib/dummy-data.ts
@@ -0,0 +1,78 @@
+import type { Player } from "./types"
+
+export const dummyRoundTitle = "Premier League"
+
+export const dummyPlayers: Player[] = [
+  {
+    id: "player-brighton",
+    name: "Brighton",
+    image: "/dummy/players/brighton.jpg",
+    teamImage: "/dummy/teams/brighton.png",
+  },
+  {
+    id: "player-manchester-city",
+    name: "Manchester City",
+    image: "/dummy/players/manchester-city.jpg",
+    teamImage: "/dummy/teams/manchester-city.png",
+  },
+  {
+    id: "player-manchester-united",
+    name: "Manchester United",
+    image: "/dummy/players/manchester-united.jpg",
+    teamImage: "/dummy/teams/manchester-united.png",
+  },
+  {
+    id: "player-chelsea",
+    name: "Chelsea",
+    image: "/dummy/players/chelsea.jpg",
+    teamImage: "/dummy/teams/chelsea.png",
+  },
+  {
+    id: "player-sunderland",
+    name: "Sunderland",
+    image: "/dummy/players/sunderland.jpg",
+    teamImage: "/dummy/teams/sunderland.png",
+  },
+  {
+    id: "player-aston-villa",
+    name: "Aston Villa",
+    image: "/dummy/players/aston-villa.jpg",
+    teamImage: "/dummy/teams/aston-villa.png",
+  },
+  {
+    id: "player-brentford",
+    name: "Brentford",
+    image: "/dummy/players/brentford.jpg",
+    teamImage: "/dummy/teams/brentford.png",
+  },
+  {
+    id: "player-bournemouth",
+    name: "Bournemouth",
+    image: "/dummy/players/bournemouth.jpg",
+    teamImage: "/dummy/teams/bournemouth.png",
+  },
+  {
+    id: "player-arsenal",
+    name: "Arsenal",
+    image: "/dummy/players/arsenal.jpg",
+    teamImage: "/dummy/teams/arsenal.png",
+  },
+  {
+    id: "player-newcastle",
+    name: "Newcastle",
+    image: "/dummy/players/newcastle.jpg",
+    teamImage: "/dummy/teams/newcastle.png",
+  },
+  {
+    id: "player-liverpool",
+    name: "Liverpool",
+    image: "/dummy/players/liverpool.jpg",
+    teamImage: "/dummy/teams/liverpool-crest.png",
+  },
+  {
+    id: "player-fulham",
+    name: "Fulham",
+    image: "/dummy/players/fulham.jpg",
+    teamImage: "/dummy/teams/fulham.png",
+  },
+]
